efad085596e1378791629d63db18bbcc0cdd9361
Chirag <163040314+jaa8chirag@users.noreply.github.com>
2026-06-18 18:00:40 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+        for(const str of strs){
+            let key = [...str].sort().join('');
+            if(!map.has(key)) map.set(key,[]);
+            map.get(key).push(str);
+        }
+        return [...map.values()]
+    }
+}
